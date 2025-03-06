a1b73b0dfc395f44b1d691c51e5102a7787d20bf
saivignesh-03 <ch.vignesh2003@gmail.com>
2025-03-05 21:34:41 -0500

    Saved local changes before pulling remote updates

--- pipelines/model_training_pipeline.py
+++ pipelines/model_training_pipeline.py
@@ -51,6 +51,5 @@ if test_mae < metric.get("test_mae"):
         input_example=features.sample(),
         model_schema=model_schema,
     )
-    model.save(model_path)
-else:
+    model.save(str(model_path))else:
     print(f"Skipping model registration because new model is not better!")
